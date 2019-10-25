4c320dcf88dac0ed362a1ad43623896f7b1c2933
crunchiersa <github@kranchie.de>
2019-10-25 23:50:35 +0100

    Changes

--- GUI_VERSIONS/src/ccnr/CCNR.java
+++ GUI_VERSIONS/src/ccnr/CCNR.java
@@ -9,69 +9,62 @@ Wert 0 erreicht wird ist die Kreditkartennummer gültig, sonst nicht.
 Das Ergebnis der Überprüfung wird ausgegeben und es wird abgefragt ob eine weitere Kreditkartennummer 
 überprüft werden soll.*/
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import javax.swing.JOptionPane;
 
 public class CCNR{
-	
+
 	public static void main(String[] args) throws IOException{
-			
+
 		//Deklaration der Variablen und Konstanten
-	    	String ccnr;
-		boolean repeat, antwort;
-		final BufferedReader eingabe = new BufferedReader(new InputStreamReader(System.in));
+		String ccnr;
+		boolean repeat, antwort, lenghtok = false;
+		//final BufferedReader eingabe = new BufferedReader(new InputStreamReader(System.in));
 		Creditcard c = new Creditcard();
-					
-		//Begrüßung im Programm
-		System.out.println("Dieses Programm dient dazu zu überprüfen, ob die eingegebenen Kreditkartennummer zulässig ist.");
-		
+
 		do{
-			//Aufforderung zur Eingabe der Kreditkartennummer.
-			System.out.print("Bitte geben Sie die Kreditkartennummer ein: ");
-			//Einlesen der Kreditkartennummer.
-			ccnr = eingabe.readLine();
-			
-			//Prüfung ob eingegebene Zahl 16 Stellen hat
-			if(ccnr.length() < 16){
-			    System.out.println("Die eingegebene Zahl ist zu kurz und daher keine gültige Kreditkartennummer.");
-	        		repeat = isMoreCredit(eingabe);
-	        		continue;
-			}else if (ccnr.length() > 16){
-			    System.out.println("Die eingegebene Zahl ist zu lang und daher keine gültige Kreditkartennummer.");
-	        		repeat = isMoreCredit(eingabe);
-	        		continue;
-			}else if (ccnr.length() == 16){
-			    antwort = c.gueltigkeitcc(ccnr);
-			    if (antwort == true) {
-				System.out.println("Die eingegebene Kreditkartennummer (" + ccnr +") ist gültig!\n\n");
-			    }else if (antwort == false) {
-				System.out.println("Die eingegebene Kreditkartennummer ist nicht gültig!\n\n");
-			    }
-			}
-			    //Abfrage ob weitere Kreditkartennummer geprüft werden soll.
-			    repeat = isMoreCredit(eingabe);								
-			}while (repeat);
+			do {
+				//Aufforderung zur Eingabe der Kreditkartennummer.
+				ccnr = JOptionPane.showInputDialog(null, "Dieses Programm dient dazu zu überprüfen, ob die eingegebenen Kreditkartennummer zulässig ist. \n Bitte geben Sie die Kreditkartennummer ein: ");
+
+				//Prüfung ob eingegebene Zahl 16 Stellen hat
+
+				if(ccnr.length() < 16){
+					JOptionPane.showMessageDialog(null, "Die eingegebene Zahl ist zu kurz und daher keine gültige Kreditkartennummer.");
+					lenghtok = false;
+					continue;
+				}else if (ccnr.length() > 16){
+					JOptionPane.showMessageDialog(null, "Die eingegebene Zahl ist zu lang und daher keine gültige Kreditkartennummer.");
+					lenghtok = false;
+					continue;
+				}else if (ccnr.length() == 16){
+					antwort = c.gueltigkeitcc(ccnr);
+					lenghtok = true;
+					if (antwort == true) {
+						JOptionPane.showMessageDialog (null, "Die eingegebene Kreditkartennummer (" + ccnr +") ist gültig!");
+					}else if (antwort == false) {
+						JOptionPane.showMessageDialog (null, "Die eingegebene Kreditkartennummer ist nicht gültig!");
+					}
+				}
+			}while (!lenghtok);
+			repeat = isMoreCredit();
+		}while (repeat);
 		
+		JOptionPane.showMessageDialog(null, "Das Programm wird nun beendet");
 	}
 
-protected static boolean isMoreCredit (final BufferedReader eingabe) throws IOException {
-    String antwort;
-    boolean repeat = true;
-    // Abfrage ob weitere Kreditkartennummer geprüft werden soll.
-	System.out.print("Möchten Sie eine weitere Kreditkartennummer überprüfen? J/N \n ");
-	antwort = eingabe.readLine();
-	switch (antwort.toUpperCase()){//Switch zu entsprechender Antwort, Konvertierung zu Großbuchstaben
-	case ("J"): repeat = true;
-	break;
-	case ("N"): repeat = false;
-	System.out.println("Auf Wiedersehen!");
-	break;
-	default: System.out.println("Ungültige Antwort!");
-	repeat = isMoreCredit(eingabe);
+	protected static boolean isMoreCredit () throws IOException {
+		boolean repeat;
+		// Abfrage ob weitere Kreditkartennummer geprüft werden soll.
+		int button = JOptionPane.YES_OPTION; 
+		int response = JOptionPane.showConfirmDialog(null, "Möchten Sie eine weitere Kreditkartennummer überprüfen?", "Weiter machen?", button);
+		if (button == response) {
+			repeat = true;
+		} else {
+			repeat = false;
+		}
+		return repeat;
 	}
-	return repeat;
 }
-}
-		
-	
+
+
